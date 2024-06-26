edc6143fd70643afcf0241b4315b4e22b774b47a
skeptrune <nick.k@trieve.ai>
2024-06-25 18:32:11 -0700

    wip: frontend update

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,9 +1,17 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import { AppContainer } from "./App";
+import { Route, Router } from "@solidjs/router";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(() => <App />, root!)
+render(
+  () => (
+    <Router>
+      <Route path="/" component={AppContainer} />
+    </Router>
+  ),
+  root!
+);
